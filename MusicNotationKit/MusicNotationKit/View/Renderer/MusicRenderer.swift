d42d173cb88fc8cd70ff5bbb2eda7358c0ead56c
Steve Barnegren <steve.barnegren@gmail.com>
2018-12-22 18:30:18 +0000

    Added Crotchet Renderer

--- MusicNotationKit/MusicNotationKit/View/Renderer/MusicRenderer.swift
+++ MusicNotationKit/MusicNotationKit/View/Renderer/MusicRenderer.swift
@@ -56,59 +56,39 @@ class MusicRenderer {
         return paths
     }
     
-    private func makeBarEndPaths(forDisplaySize displaySize: DisplaySize) -> [Path] {
-        
-        let midY = displaySize.height/2
-        let numberOfLines = 5
-        let staveHeight = Double(numberOfLines-1) * staveSpacing
-        
-        // Draw the left line
-        var leftPath = Path()
-        leftPath.move(to: Point(0, midY - staveHeight/2))
-        leftPath.addLine(to: Point(0, midY + staveHeight/2))
-        
-        // Draw the right line
-        var rightPath = Path()
-        rightPath.move(to: Point(displaySize.width, midY - staveHeight/2))
-        rightPath.addLine(to: Point(displaySize.width, midY + staveHeight/2))
-        
-        return [leftPath, rightPath]
-        
-    }
-    
     private func makePaths(forTokens positionedTokens: [PositionedToken], centerY: Double) -> [Path] {
         
         var paths = [Path]()
         
         for positionedToken in positionedTokens {
-            paths.append(makePath(forToken: positionedToken.token,
-                                  centerY: centerY,
-                                  xPos: positionedToken.xPos * staveSpacing))
+            paths += makePaths(forToken: positionedToken.token,
+                               centerY: centerY,
+                               xPos: positionedToken.xPos * staveSpacing)
         }
         
         return paths
     }
     
-    func makePath(forToken token: Token, centerY: Double, xPos: Double) -> Path {
+    func makePaths(forToken token: Token, centerY: Double, xPos: Double) -> [Path] {
         
         switch token {
         case .semibreve(let pitch):
-            return makeNotePath(fromSymbolPath: SymbolPaths.filledNoteHead,
-                                centerY: centerY,
-                                staveOffset: pitch.staveOffset,
-                                xPos: xPos)
+            return [makeNotePath(fromSymbolPath: SymbolPaths.filledNoteHead,
+                                 centerY: centerY,
+                                 staveOffset: pitch.staveOffset,
+                                 xPos: xPos)]
         case .crotchet(let pitch):
-            return makeNotePath(fromSymbolPath: SymbolPaths.filledNoteHead,
-                                centerY: centerY,
-                                staveOffset: pitch.staveOffset,
-                                xPos: xPos)
+            return CrotchetRenderer.crotchetPaths(withPitch: pitch,
+                                                  staveCenterY: centerY,
+                                                  xPos: xPos,
+                                                  staveSpacing: staveSpacing)
         case .minim(let pitch):
-            return makeNotePath(fromSymbolPath: SymbolPaths.openNoteHead,
-                                centerY: centerY,
-                                staveOffset: pitch.staveOffset,
-                                xPos: xPos)
+            return [makeNotePath(fromSymbolPath: SymbolPaths.openNoteHead,
+                                 centerY: centerY,
+                                 staveOffset: pitch.staveOffset,
+                                 xPos: xPos)]
         case .barline:
-            return makeBarlinePath(centerY: centerY, xPos: xPos)
+            return [makeBarlinePath(centerY: centerY, xPos: xPos)]
         }
     }
     
