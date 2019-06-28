5f7cfc0ff0b98bee5b99908400d9824ab1917e4e
Steve Barnegren <steve.barnegren@gmail.com>
2019-06-28 20:50:55 +0100

    Remove old layout system

--- MusicNotationKit/MusicNotationKit/Renderer/Render Operations/Horizontal Positioner/HorizontalPositionerRenderOperation.swift
+++ MusicNotationKit/MusicNotationKit/Renderer/Render Operations/Horizontal Positioner/HorizontalPositionerRenderOperation.swift
@@ -12,69 +12,7 @@ class HorizontalPositionerRenderOperation: RenderOperation {
     
     func process(composition: Composition, layoutWidth: Double) {
         
-        // Make the Items
-        let items = composition.bars.map { constrainedItems(forBar: $0) }.joined().toArray()
-        
         let anchors = LayoutAnchorsBuilder().makeAnchors(from: composition)
         HorizontalLayoutSolver().solve(anchors: anchors, layoutWidth: layoutWidth)
-        
-        //let distances = HorizontalDistancesGenerator().makeConstrainedDistances(fromItems: items)
-        //HorizontalConstraintSolver().solve(distances, layoutWidth: layoutWidth)
-    }
-    
-    private func constrainedItems(forBar bar: Bar) -> [HorizontalLayoutItem] {
-        
-        // We'll just process a single note sequence for now
-        var items = [HorizontalLayoutItem]()
-        items.append(bar.leadingBarline)
-        items.append(makePostBarlineSpacer())
-        
-        for (playableItem, isLast) in bar.sequences[0].playables.enumeratedWithLastItemFlag() {
-            //items.append(contentsOf: playableItem.horizontallyConstrainedItems)
-            items.append(playableItem)
-            items.append(isLast ? makeLastNoteSpacer() : makePostNoteSpacer())
-        }
-        
-        return items
-    }
-    
-    private func makePostBarlineSpacer() -> Spacer {
-        
-        let spacer = Spacer()
-        spacer.add(value: ConstraintValue(length: 1, priority: ConstraintPriority.regular))
-        return spacer
-    }
-    
-    private func makePostNoteSpacer() -> Spacer {
-        
-        let spacer = Spacer()
-        spacer.add(value: ConstraintValue(length: 0.5, priority: ConstraintPriority.regular))
-        return spacer
-    }
-    
-    private func makeLastNoteSpacer() -> Spacer {
-        
-        let spacer = Spacer()
-        spacer.add(value: ConstraintValue(length: 1, priority: ConstraintPriority.regular))
-        return spacer
-    }
-}
-
-fileprivate class Spacer: HorizontalLayoutItem {
-    
- 
-    var layoutDuration: Time? = nil
-    var xPosition: Double = 0
-    
-    let leadingConstraint = HorizontalConstraint.zero
-    var trailingConstraint: HorizontalConstraint {
-        return HorizontalConstraint(values: constraintValues)
-    }
-    let trailingLayoutItems = [HorizontalLayoutItem]()
-    
-    private var constraintValues = [ConstraintValue]()
-    
-    func add(value: ConstraintValue) {
-        constraintValues.append(value)
     }
 }
