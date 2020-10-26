c1c670eb171b7cadf61b9e6f31eb2a89a9be2b88
Steve Barnegren <steve.barnegren@gmail.com>
2020-10-26 20:52:08 +0000

    Added ties component composition example conflicting with accents

--- Example/Example/Components Menu/Component Compositions/ComponentCompositions+Ties.swift
+++ Example/Example/Components Menu/Component Compositions/ComponentCompositions+Ties.swift
@@ -172,101 +172,22 @@ extension ComponentCompositions {
             }
         }
         
-        return compostion
-    }
-}
-
-/*
-extension ComponentCompositions {
-    
-    static var ties: Composition {
-        
-        let compostion = Composition()
+        // Avoid Accents
         
         do {
-            let pitches: [Pitch] = [.e3, .g3, .b4, .d4]
             
-            do {
-                let bar = Bar()
-                let sequence = NoteSequence()
-                
-                sequence.add(rest: Rest(value: .dottedQuaver))
-                sequence.add(note: Note(value: .semiquaver, pitches: pitches).tied())
-                //sequence.add(rest: Rest(value: .semiquaver))
-
-                sequence.add(note: Note(value: .crotchet, pitches: pitches).tied())
-                sequence.add(note: Note(value: .crotchet, pitches: pitches))
-
-                bar.add(sequence: sequence)
-                compostion.add(bar: bar)
-            }
+            let bar = Bar()
+            let sequence = NoteSequence()
             
-//            do {
-//                let bar = Bar()
-//                let sequence = NoteSequence()
-//
-//                sequence.add(note: Note(value: .minim, pitches: pitches).tied())
-//                sequence.add(note: Note(value: .quaver, pitches: pitches).tied())
-//
-//                bar.add(sequence: sequence)
-//                compostion.add(bar: bar)
-//            }
+            sequence.add(note: Note(value: .crotchet, pitch: .d4).tied().accent())
+            sequence.add(note: Note(value: .crotchet, pitch: .d4).accent())
+            sequence.add(note: Note(value: .crotchet, pitch: .e4).tied().accent())
+            sequence.add(note: Note(value: .crotchet, pitch: .e4).accent())
+            
+            bar.add(sequence: sequence)
+            compostion.add(bar: bar)
         }
         
         return compostion
     }
 }
-*/
-/*
-extension ComponentCompositions {
-    
-    static var ties: Composition {
-        
-        let compostion = Composition()
-        
-        let bar = Bar()
-        let sequence = NoteSequence()
-        
-        sequence.add(rest: Rest(value: .dottedQuaver))
-        sequence.add(note: Note(value: .semibreve, pitches: [.g3, .b4]).tied())
-        sequence.add(note: Note(value: .crotchet, pitches: [.g3, .b4]))
-        
-        bar.add(sequence: sequence)
-        compostion.add(bar: bar)
-        
-        return compostion
-    }
-}
-*/
-/*
-extension ComponentCompositions {
-    
-    static var ties: Composition {
-        
-        let compostion = Composition()
-        
-        let bar = Bar()
-        
-        let topSequence = NoteSequence()
-        topSequence.add(note: Note(value: .quaver, pitch: .g4).tied())
-        topSequence.add(note: Note(value: .quaver, pitch: .a4))
-        topSequence.add(note: Note(value: .crotchet, pitch: .g4))
-        
-        let middleSequence = NoteSequence()
-        middleSequence.add(note: Note(value: .crotchet, pitch: .c4).tied())
-        middleSequence.add(note: Note(value: .semiquaver, pitch: .c4))
-        middleSequence.add(rest: Rest(value: .semiquaver))
-        middleSequence.add(rest: Rest(value: .quaver))
-        
-        let bottomSequence = NoteSequence()
-        bottomSequence.add(note: Note(value: .minim, pitch: .f3))
-        
-        bar.add(sequence: topSequence)
-        bar.add(sequence: middleSequence)
-        bar.add(sequence: bottomSequence)
-        compostion.add(bar: bar)
-        
-        return compostion
-    }
-}
-*/
