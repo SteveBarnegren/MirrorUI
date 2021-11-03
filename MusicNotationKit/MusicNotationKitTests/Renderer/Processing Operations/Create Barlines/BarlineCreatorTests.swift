dd55fb112b2640f61916fafd6dfaee510c8dd8af
Steve Barnegren <steve.barnegren@gmail.com>
2021-11-03 19:33:26 +0000

    Added BarlineCreator tests

---
+++ MusicNotationKit/MusicNotationKitTests/Renderer/Processing Operations/Create Barlines/BarlineCreatorTests.swift
@@ -0,0 +1,66 @@
+import XCTest
+@testable import MusicNotationKit
+
+class BarlineCreatorTests: XCTestCase {
+
+    private let barlineCreator = BarlineCreator()
+
+    // MARK: - Single Line
+
+    func test_DefaultToSingleLine() {
+        verify(preceedingOptions: [],
+               trailingOptions: [],
+               expectedLineType: .single)
+    }
+
+    // MARK: - Double line
+
+    func test_LeadingDoubleLineIgnored() {
+        verify(preceedingOptions: [.double],
+               trailingOptions: [],
+               expectedLineType: .single)
+    }
+
+    func test_TrailingDouble() {
+        verify(preceedingOptions: [],
+               trailingOptions: [.double],
+               expectedLineType: .double)
+    }
+
+    // MARK: - Final line
+
+    func test_LeadingFinalLine() {
+        verify(preceedingOptions: [.final],
+               trailingOptions: [],
+               expectedLineType: .final)
+    }
+
+    func test_TrailingFinalLineIgnored() {
+        verify(preceedingOptions: [],
+               trailingOptions: [.final],
+               expectedLineType: .single)
+    }
+
+    func test_FinalLineTakesPrecedenceOverDouble() {
+        verify(preceedingOptions: [.final],
+               trailingOptions: [.double],
+               expectedLineType: .final)
+    }
+}
+
+// MARK: - Verification
+
+extension BarlineCreatorTests {
+
+    private func verify(preceedingOptions: BarlineOptions,
+                        trailingOptions: BarlineOptions,
+                        expectedLineType: Barline.LineType,
+                        file: StaticString = #file,
+                        line: UInt = #line) {
+
+        let barline = barlineCreator.createBarline(leadingOptions: preceedingOptions,
+                                                   trailingOptions: trailingOptions)
+        XCTAssertEqual(barline.lineType, expectedLineType, file: file, line: line)
+    }
+
+}
